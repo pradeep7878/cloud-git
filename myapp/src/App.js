604e9cc9cbdf9f648f21529f5c781d451e5025d6
Pradeep <pradivpj@gmail.com>
2024-02-07 17:32:29 +0530

    Heading changed

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,10 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
       <>
-        <h1>Github Learning</h1>
+        <h1>Cloud git - Github</h1>
       </>
   );
 }
